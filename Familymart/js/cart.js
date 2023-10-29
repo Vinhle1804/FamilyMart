24795b81ef65406f7d732d6df3e43332ded12d62
Vinhle1804 <131293438+Vinhle1804@users.noreply.github.com>
2023-10-29 14:30:36 +0700

    Add files via upload

--- Familymart/js/cart.js
+++ Familymart/js/cart.js
@@ -1,17 +1,18 @@
-
-
-
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function tinhtongtien() {
-  let total = cart.reduce((acc, curr) => acc + curr.quantity * curr.price, 0);
+  let total = cart.reduce((acc, curr) => acc + curr.quantity * curr.price, 0).toLocaleString() + "đ";
   document.getElementById("totall").innerHTML = total;
 }
 
-function removeCart(id){
-  cart.splice(id, 1);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  renderCart();
+function removeCart(id) {
+  const index = cart.findIndex(item => item.id === id);
+  if (index !== -1) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
+    tinhtongtien();
+  }
 }
 
 function renderCart() {
@@ -19,13 +20,13 @@ function renderCart() {
   for (let i = 0; i < cart.length; i++) {
     element += `
       <tr>
-        <td>1</td>
+        <td>${i + 1}</td>
         <td><img src="${cart[i].image}" alt="Product 1"></td>
         <td>${cart[i].name}</td>
         <td>${cart[i].price.toLocaleString()}đ</td>
         <td><input type="number" class="quantity" value="${cart[i].quantity}" data-product-id="${cart[i].id}" onchange="updateQuantity(${i}, this.value)"></td>
         <td>${(cart[i].price * cart[i].quantity).toLocaleString()}đ</td>
-        <td><button class="button" onClick="removeCart(${cart[i].id})">removecart</button></td>
+        <td><button class="button" onClick="removeCart(${cart[i].id})"><i class='bx bx-trash'></i></button></td>
       </tr>
     `;
   }
@@ -34,10 +35,14 @@ function renderCart() {
 }
 
 function updateQuantity(index, newQuantity) {
+  if (newQuantity < 1) {
+    newQuantity = 1;
+  }
   cart[index].quantity = parseInt(newQuantity);
   localStorage.setItem("cart", JSON.stringify(cart));
   renderCart();
   tinhtongtien();
 }
+
 renderCart();
 tinhtongtien();
--- Familymart/js/index.js
+++ Familymart/js/index.js
@@ -1,119 +1,131 @@
-  const newproducts = [
-    {
-      id: 1,
-      image: "../image/sp1.jpg",
-      category: "cháo",
-      name: "Cháo nấm đùi gà",
-      price: 19990,
-    },
-    {
-      id: 2,
-      image: "../image/sp2.jpg",
-      category: "cơm nắm",
-      name: "Cơm nắm tép sấy",
-      price: 29999,
-    },
-    {
-      id: 3,
-      image: "../image/sp3.jpg",
-      category: "mì",
-      name: "Mì xá xíu hoành thánh tôm thịt",
-      price: 24999,
-    },
-    {
-      id: 4,
-      image: "../image/sp4.jpg",
-      category: "dimsum",
-      name: "Dimsum tôm thịt",
-      price: 39999,
-    },
-    {
-      id: 5,
-      image: "../image/sp1.jpg",
-      category: "Túi xách",
-      name: "Túi xách da thật",
-      price: 49999,
-    },
-    {
-      id: 6,
-      image: "../image/sp1.jpg",
-      category: "Áo",
-      name: "Áo khoác nam",
-      price: 59999,
-    },
-    {
-      id: 7,
-      image: "../image/sp1.jpg",
-      category: "Giày",
-      name: "Giày thể thao nữ",
-      price: 44999,
-    },
-    {
-      id: 8,
-      image: "../image/sp1.jpg",
-      category: "Túi xách",
-      name: "Túi xách đựng laptop",
-      price: 69999,
-    },
-  ];
-  const hightlightproducts = [
-    {
-      id: 9,
-      image: "../image/sp5.png",
-      category: "banhmi",
-      name: "banhmi hoacuc",
-      price: 19999,
-    },
-    {
-      id: 10,
-      image: "../image/sp6.png",
-      category: "Quần",
-      name: "Quần jean nam",
-      price: 29999,
-    },
-    {
-      id: 11,
-      image: "../image/sp7.png",
-      category: "Áo",
-      name: "Áo sơ mi nữ",
-      price: 24999,
-    },
-    {
-      id: 12,
-      image: "../image/sp8.png",
-      category: "Giày",
-      name: "Giày thể thao",
-      price: 39999,
-    },
-    {
-      id: 13,
-      image: "../image/sp1.jpg",
-      category: "Túi xách",
-      name: "Túi xách da thật",
-      price: 49999,
-    },
-    {
-      id: 14,
-      image: "../image/sp1.jpg",
-      category: "Áo",
-      name: "Áo khoác nam",
-      price: 59999,
-    },
-    {
-      id: 15,
-      image: "../image/sp1.jpg",
-      category: "Giày",
-      name: "Giày thể thao nữ",
-      price: 44999,
-    },
-    {
-      id: 16,
-      image: "../image/sp1.jpg",
-      category: "Túi xách",
-      name: "Túi xách đựng laptop",
-      price: 69999,
-    },
-  ];
+const newproducts = [
+  {
+    id: 1,
+    image: '../image/sp1.jpg',
+    href: '../views/products/ChaoNamDuiGa.html',
+    category: 'cháo',
+    name: 'Cháo nấm đùi gà',
+    price: 19990,
+  },
+  {
+    id: 2,
+    image: '../image/sp2.jpg',
+    href: '../views/products/ComNamTepSay.html',
+    category: 'cơm nắm',
+    name: 'Cơm nắm tép sấy',
+    price: 29999,
+  },
+  {
+    id: 3,
+    image: '../image/sp3.jpg',
+    href: '../views/products/MiXaXiuHoanhThanhTomThit.html',
+    category: 'mì',
+    name: 'Mì xá xíu hoành thánh tôm thịt',
+    price: 24999,
+  },
+  {
+    id: 4,
+    image: '../image/sp4.jpg',
+    href: '../views/products/DimsumTomThi.html',
+    category: 'dimsum',
+    name: 'Dimsum tôm thịt',
+    price: 39999,
+  },
+  {
+    id: 5,
+    image: '../image/sp1.jpg',
+    href: '../views/products/DimsumTomThi.html',
+    category: 'Túi xách',
+    name: 'Túi xách da thật',
+    price: 49999,
+  },
+  {
+    id: 6,
+    image: '../image/sp1.jpg',
+    href: '../views/products/DimsumTomThi.html',
+    category: 'Áo',
+    name: 'Áo khoác nam',
+    price: 59999,
+  },
+  {
+    id: 7,
+    image: '../image/sp1.jpg',
+    href: '../views/products/DimsumTomThi.html',
+    category: 'Giày',
+    name: 'Giày thể thao nữ',
+    price: 44999,
+  },
+  {
+    id: 8,
+    image: '../image/sp1.jpg',
+    href: '../views/products/DimsumTomThi.html',
+    category: 'Túi xách',
+    name: 'Túi xách đựng laptop',
+    price: 69999,
+  },
+];
+const hightlightproducts = [
+  {
+    id: 9,
+    image: '../image/sp5.png',
+    href: '../views/products/BanhMiHoaCuc.html',
+    category: 'banhmi',
+    name: 'banhmi hoacuc',
+    price: 19999,
+  },
+  {
+    id: 10,
+    image: '../image/sp6.png',
+    href: '../views/products/QuanJeanNam.html',
+    category: 'Quần',
+    name: 'Quần jean nam',
+    price: 29999,
+  },
+  {
+    id: 11,
+    image: '../image/sp7.png',
+    href: '../views/products/AoSoMiNu.html',
+    category: 'Áo',
+    name: 'Áo sơ mi nữ',
+    price: 24999,
+  },
+  {
+    id: 12,
+    image: '../image/sp8.png',
+    href: '../views/products/GiayTheThao.html',
+    category: 'Giày',
+    name: 'Giày thể thao',
+    price: 39999,
+  },
+  {
+    id: 13,
+    image: '../image/sp1.jpg',
+    category: 'Túi xách',
+    name: 'Túi xách da thật',
+    price: 49999,
+  },
+  {
+    id: 14,
+    image: '../image/sp1.jpg',
+    category: 'Áo',
+    name: 'Áo khoác nam',
+    price: 59999,
+  },
+  {
+    id: 15,
+    image: '../image/sp1.jpg',
+    category: 'Giày',
+    name: 'Giày thể thao nữ',
+    price: 44999,
+  },
+  {
+    id: 16,
+    image: '../image/sp1.jpg',
+    category: 'Túi xách',
+    name: 'Túi xách đựng laptop',
+    price: 69999,
+  },
+];
 
 
   function addCart(id) {
@@ -134,6 +146,9 @@
   }
 
 
+ 
+
+
 
 
   function renderProduct(products, containerId) {
@@ -141,13 +156,13 @@
     for (let i = 0; i < products.length; i++) {
       element += `
         <div class="product">
-          <img src="${products[i].image}" alt="${products[i].name}">
+        <a href="${products[i].href}"><img src="${products[i].image}" alt="${products[i].name}"></a>
           <h5 style="color: rgb(118, 129, 129);">${products[i].category}</h5>
           <hr style="width: 50px;">
           <h3 style="text-transform: uppercase; color: rgb(52, 93, 170); white-space: nowrap;
               overflow: hidden; text-overflow: ellipsis; width: 300px;">${products[i].name}</h3>
-          <h2>${products[i].price}đ</h2>
-          <button class="button" onClick="addCart(${products[i].id})">Add to cart</button>
+          <h2>${(products[i].price).toLocaleString()}đ</h2>
+          <button class="button" id="addCart" onClick="addCart(${products[i].id})">Add to cart</button>
         </div>
       `;
     }
@@ -157,5 +172,30 @@
   const newProductsToShow = newproducts.slice(0, 4);
   const highlightProductsToShow = hightlightproducts.slice(0, 4);
 
+
   renderProduct(newProductsToShow, "newproducts");
   renderProduct(highlightProductsToShow, "hightlightproducts");
+
+
+
+  function hiLogin(){
+    let userInfor = JSON.parse(localStorage.getItem("userInfor"));
+    let element = "Hi " + userInfor.fullname + ",";
+    // for (let i = 0; i < getLogin.length; i++) {
+    //     element += `
+    //         <li><button type="button" id="get" style="color: orange; margin-left: 5px;"><p>Hi ${getLogin[i].username},</p></button></li>
+    //     `;
+    // }
+    document.getElementById('get').innerHTML = element;
+}
+
+hiLogin()
+
+function remove() {
+  let userInfor = JSON.parse(localStorage.removeItem("userInfor"));
+    let element = "đăng nhập" +userInfor.fullname;
+
+    document.getElementById('remove').innerHTML = element;
+}
+remove()
+
